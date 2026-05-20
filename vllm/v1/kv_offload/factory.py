d868f7b2e9cbcd25d5124d041ee3199300ee57fa
yxstev <zhangyixiang9@huawei.com>
2026-05-20 15:30:02 +0800

    [DYCP] fix CrossDPExampleConnector registration

--- vllm/v1/kv_offload/factory.py
+++ vllm/v1/kv_offload/factory.py
@@ -56,9 +56,3 @@ class OffloadingSpecFactory:
 OffloadingSpecFactory.register_spec(
     "CPUOffloadingSpec", "vllm.v1.kv_offload.cpu", "CPUOffloadingSpec"
 )
-
-KVConnectorFactory.register_connector(
-    "CrossDPExampleConnector",
-    "vllm.distributed.kv_transfer.kv_connector.v1.cross_dp_example_connector",
-    "CrossDPExampleConnector",
-)
